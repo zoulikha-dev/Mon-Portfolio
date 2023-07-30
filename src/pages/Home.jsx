dfd84bc8789a508de5091b4434c48e50f5630cc3
zoulikha <zaz13@me.com>
2023-07-30 12:34:23 +0200

    Implemented background

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/components/home.scss";
+
+const Home = () => {
+  return <div className="home-container">{/* Votre contenu ici */}</div>;
+};
+
+export default Home;
