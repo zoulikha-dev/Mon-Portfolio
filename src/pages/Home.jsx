59b679d0e7952674f1715a4b4deb3c14ebc7ddde
zoulikha <zaz13@me.com>
2023-07-31 01:15:38 +0200

    add typewriter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "../styles/components/home.scss";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
-  return <div className="home-container">{/* Votre contenu ici */}</div>;
+  return (
+    <div className="background-container">
+      <div className="typewriter-container">
+        <h1>
+          <Typewriter
+            options={{
+              strings: "Je suis Zoulikha EL HADRI",
+              autoStart: true,
+            }}
+          ></Typewriter>
+        </h1>
+        <h2>
+          <Typewriter
+            options={{
+              strings: "Développeuse Web",
+              autoStart: true,
+              loop: true,
+            }}
+          ></Typewriter>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
