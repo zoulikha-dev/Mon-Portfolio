5064b3335d4231e09e05c3f911fe6a4d08c86e4e
zoulikha <zaz13@me.com>
2023-08-02 23:39:04 +0200

    edit app.js (single page app)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Skills from "./pages/Skills";
-import Project from "./pages/Project";
-import Contact from "./pages/Contact";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -16,10 +12,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Home />} />
-          <Route path="/presentation" element={<About />} />
-          <Route path="/competences" element={<Skills />} />
-          <Route path="/portfolio" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
       <Footer />
