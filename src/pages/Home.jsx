287e2be5d71c9c726c908d7d7e980a6aab4b650d
zoulikha <zaz13@me.com>
2023-08-07 03:09:58 +0200

    add component modal, create form, edit compétences

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,17 @@ import {
   FaNodeJs,
   FaGithub,
   FaSass,
-  FaGlobe,
+  FaChevronDown,
 } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import booki from "../assets/img/booki1.png";
 import kasa from "../assets/img/kasa.png";
+import bookiModal from "../assets/img/Mobile.png";
+import bookiModal2 from "../assets/img/Tablette.webp";
+import kasaModal from "../assets/img/D_Home1.webp";
+import kasaModal2 from "../assets/img/D_Fiche-Logement.webp";
 import Icons from "../components/Icons";
+import Modalcomponent from "../components/Modalcomponent";
 
 const Home = () => {
   // Déclaration du state projets pour stocker les donées du fichier JSON
@@ -37,193 +42,179 @@ const Home = () => {
   const secondProject = projets && projets.length > 1 ? projets[1] : null;
 
   return (
-    <main>
-      {/* ------------------------- Accueil ------------------------------- */}
-      <section id="accueil" className="background-container">
-        <div className="msg-container">
-          <div className="typewriter-container">
-            <h1 className="title-main">
-              <Typewriter
-                options={{
-                  strings: "Je suis Zoulikha EL HADRI",
-                  autoStart: true,
-                }}
-              ></Typewriter>
-            </h1>
-            <h2 className="subtitle">
-              <Typewriter
-                options={{
-                  strings: "Développeuse Web",
-                  autoStart: true,
-                  loop: true,
-                }}
-              ></Typewriter>
-            </h2>
+    <>
+      <main>
+        {/* ------------------------- Accueil ------------------------------- */}
+        <section id="accueil" className="background-container">
+          <div className="msg-container">
+            <div className="typewriter-container">
+              <h1 className="title-main">
+                <Typewriter
+                  options={{
+                    strings: "Je suis Zoulikha EL HADRI",
+                    autoStart: true,
+                  }}
+                ></Typewriter>
+              </h1>
+              <h2 className="subtitle">
+                <Typewriter
+                  options={{
+                    strings: "Développeuse Web",
+                    autoStart: true,
+                    loop: true,
+                  }}
+                ></Typewriter>
+              </h2>
+            </div>
+            <div className="arrow-container">
+              <a href="#portfolio">
+                <FaChevronDown className="fade" />
+              </a>
+            </div>
           </div>
-          {/* <div className="btn">
-          <button>a voir plus tard !!!!!!</button>
-        </div> */}
-        </div>
-      </section>
-      {/* --------------------------- Présentation ---------------------------------- */}
-      <section id="presentation" className="about-container">
-        <img src={photo} alt="Ma photo" className="photo" />
-        <h2>Présentation</h2>
-        <p>
-          Passionnée par le développement web, j'ai décidé de me former chez
-          OpenClassRooms pour concrétiser mon désir de devenir développeuse web.
-          <br /> Au cours de ma formation, j'ai acquis des connaissances
-          approfondies en HTML, CSS, JavaScript, et React pour concevoir des
-          sites web attrayants et réactifs. De plus, j'ai découvert le
-          développement back-end en utilisant NodeJS, Express, et MongoDB, ce
-          qui me permet de progresser dans la création d'applications web
-          complètes.
-          <br />
-          <br></br>
-          Je suis enthousiaste à l'idée de mettre en pratique ces compétences et
-          de contribuer à des projets web passionnants. Mon objectif est de
-          continuer à évoluer professionnellement dans le domaine du
-          développement web.
-        </p>
-        <section className="presentation-icons">
-          <Icons colorClass="icons-presentation" />
         </section>
-      </section>
+        {/* --------------------------- Présentation ---------------------------------- */}
+        <section id="presentation" className="about-container">
+          <img src={photo} alt="Ma photo" className="photo" />
+          <h2>Présentation</h2>
+          <p>
+            Passionnée par le développement web, j'ai décidé de me former chez
+            OpenClassRooms pour concrétiser mon désir de devenir développeuse
+            web.
+            <br /> Au cours de ma formation, j'ai acquis des connaissances
+            approfondies en HTML, CSS, JavaScript, et React pour concevoir des
+            sites web attrayants et réactifs. De plus, j'ai découvert le
+            développement back-end en utilisant NodeJS, Express, et MongoDB, ce
+            qui me permet de progresser dans la création d'applications web
+            complètes.
+            <br />
+            <br></br>
+            Je suis enthousiaste à l'idée de mettre en pratique ces compétences
+            et de contribuer à des projets web passionnants. Mon objectif est de
+            continuer à évoluer professionnellement dans le domaine du
+            développement web.
+          </p>
+          <section className="presentation-icons">
+            <Icons colorClass="icons-presentation" />
+          </section>
+        </section>
 
-      {/* ---------------------------- Compétences ------------------------------- */}
-      <section id="competences" className="skills-container">
-        <h2 className="skills-title">Mes compétences</h2>
-        <div className="icon-skills">
-          <div className="first-icon">
-            <FaHtml5 className="icon-html" />
-            <FaCss3Alt className="icon-css" />
-            <FaSass className="icon-sass" />
-            <FaJs className="icon-js" />
-            <FaReact className="icon-react" />
+        {/* ---------------------------- Compétences ------------------------------- */}
+        <section id="competences" className="skills-container">
+          <h2 className="skills-title">Mes compétences</h2>
+          <div className="icon-skills">
+            <div className="first-icon">
+              <FaHtml5 className="icon-html" />
+              <FaCss3Alt className="icon-css" />
+              <FaSass className="icon-sass" />
+              <FaJs className="icon-js" />
+              <FaReact className="icon-react" />
+            </div>
+            <p>
+              Construction de sites web responsive et dynamique grâce à HTML,
+              CSS, JavaScript et React. Optimisation des performances et
+              maintenance de sites existants garantissant leur bon
+              fonctionnement et une meilleure visibilité sur les moteurs de
+              recherche.
+            </p>
+            <FaNodeJs className="icon-nodejs" />
+            <p>
+              Création d'API et de bases de données pour concevoir des sites
+              complets et dynamiques, assurant ainsi leur bon fonctionnement
+              côté serveur. Utilisation des technologies telles que NodeJS,
+              Express, et MongoDB pour mettre en place des solutions robustes et
+              performantes.
+            </p>
+            <FaGithub className="icon-github1" />
+            <p>
+              Utilisation de Git et Github pour versionner mon code, suivre les
+              modifications apportés et collaborer en équipe de manière
+              transparente.
+            </p>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
-            consequuntur voluptates doloremque accusamus! Natus repellendus quod
-            suscipit iste amet, laudantium, aperiam voluptatem tempore delectus
-            fuga quasi placeat a commodi. Doloremque!
-          </p>
-          <FaNodeJs className="icon-nodejs" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vitae
-            nostrum numquam mollitia, soluta ex non perferendis eveniet magni
-            debitis vero voluptates, autem dolorem iste totam sapiente impedit
-            ipsam ipsum aliquam optio doloribus omnis quasi! Tempore sit
-            voluptates sint odit ut, temporibus possimus porro voluptate ipsum,
-            unde, accusantium nam iste?
-          </p>
-          <FaGithub className="icon-github1" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae
-            quisquam obcaecati odit repudiandae quam illum nihil voluptatem,
-            natus earum a.
-          </p>
-        </div>
-      </section>
+        </section>
 
-      {/* ---------------------------- Portfolio ------------------------- */}
-      <section id="portfolio" className="project-container">
-        <h2 className="title">Mes projets</h2>
-        <div className="img-container">
-          <img src={booki} alt="Booki" className="img-booki" />
-          <div className="projet-details">
-            {firstProject && (
-              <div>
-                <h3>{firstProject.titre}</h3>
-                <p>{firstProject.description}</p>
-                {/* liste des compétences */}
-                <div className="competences-container">
-                  <h4>Langages utilisés</h4>
-                  <ul className="competence-ul">
-                    {/* Utilisation de la méthode map pour parcourir le tableau des compétences */}
-                    {firstProject.competences.map((competence, index) => (
-                      // Utilisation de la clé unique "index" pour chaque élément de la liste
-                      <li className="competence-li" key={competence + index}>
-                        {competence}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                {/* icone lien site */}
-                <h4 className="title-lien">Lien du site et du GitHub</h4>
-                <div className="lien-icons">
-                  <a
-                    href={firstProject.lienSite}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGlobe className="icon-globe" />
-                  </a>
-                  {/* icone lien github */}
-                  <a
-                    href={firstProject.lienGitHub}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGithub className="icon-github" />
-                  </a>
-                </div>
-                {/* fin icones */}
+        {/* ---------------------------- Portfolio ------------------------- */}
+        <section id="portfolio" className="project-container">
+          <h2 className="title-container">Mes projets</h2>
+          <div className="img-container">
+            <div className="container-bis">
+              <img src={booki} alt="Booki" className="img-booki" />
+              <div className="modal">
+                {firstProject && (
+                  <Modalcomponent
+                    titleModal={firstProject.titre}
+                    imageModal={bookiModal}
+                    imageModal2={bookiModal2}
+                    description={firstProject.description}
+                    title2={firstProject.sousTitre}
+                    title3={firstProject.sousTitre2}
+                    competences={firstProject.competences}
+                    lienSite={firstProject.lienSite}
+                    lienGitHub={firstProject.lienGitHub}
+                  />
+                )}
               </div>
-            )}
-          </div>
-          {/* ----------------------- PROJET NUMÉRO 2 -------------------------------------- */}
-
-          <img src={kasa} alt="kasa" className="img-kasa" />
-
-          <div className="projet-details">
-            {secondProject && (
-              <div>
-                <h3>{secondProject.titre}</h3>
-                <p>{secondProject.description}</p>
-                {/* liste des compétences */}
-                <div className="competences-container">
-                  <h4>Langages utilisés</h4>
-                  <ul className="competence-ul">
-                    {/* Utilisation de la méthode map pour parcourir le tableau des compétences */}
-                    {secondProject.competences.map((competence, index) => (
-                      // Utilisation de la clé unique "index" pour chaque élément de la liste
-                      <li className="competence-li" key={competence + index}>
-                        {competence}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                {/* icone lien site */}
-                <h4 className="title-lien">Lien du site et du GitHub</h4>
-                <div className="lien-icons">
-                  <a
-                    href={secondProject.lienSite}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGlobe className="icon-globe" />
-                  </a>
-                  {/* icone lien github */}
-                  <a
-                    href={secondProject.lienGitHub}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGithub className="icon-github" />
-                  </a>
-                </div>
-                {/* fin icones */}
+            </div>
+            <div className="container-bis">
+              <img src={kasa} alt="kasa" className="img-kasa" />
+              <div className="modal">
+                {secondProject && (
+                  <Modalcomponent
+                    titleModal={secondProject.titre}
+                    imageModal={kasaModal}
+                    imageModal2={kasaModal2}
+                    description={secondProject.description}
+                    title2={secondProject.sousTitre}
+                    title3={secondProject.sousTitre2}
+                    competences={secondProject.competences}
+                    lienGitHub={secondProject.lienGitHub}
+                  />
+                )}
               </div>
-            )}
+            </div>
           </div>
-        </div>
-      </section>
-      <section id="contact">
-        <div>
-          <h2>contact a faire</h2>
-        </div>
-      </section>
-    </main>
+        </section>
+
+        {/* ------------------------------ Contact -------------------------- */}
+
+        <section className="contact">
+          <h2>N'hésitez pas à me contacter</h2>
+          <form action="#">
+            <div className="input-container">
+              <label for="name">Nom</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Nom"
+                required
+              />
+              <label for="email">Adresse e-mail</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Adresse e-mail"
+                required
+              />
+              <label for="message">Votre message</label>
+              <textarea
+                name="message"
+                id="message"
+                cols="30"
+                rows="10"
+                placeholder="Votre message"
+                required
+              ></textarea>
+              <button type="submit" className="btn-contacter">
+                Me contacter
+              </button>
+            </div>
+          </form>
+        </section>
+      </main>
+    </>
   );
 };
 
