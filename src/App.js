35149957dc541e4d6a229ed7ca6520653d028ec9
zoulikha <zaz13@me.com>
2023-08-08 13:37:25 +0200

    edit header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Home />} />
