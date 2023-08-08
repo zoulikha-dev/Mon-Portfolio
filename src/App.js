2af8f1e6aa5feda9f3a0f4ddb50cca3964e64a8a
zoulikha <zaz13@me.com>
2023-08-08 14:28:11 +0200

    add error pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Erreur404 from "./pages/Error404";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Erreur404 />} />
         </Routes>
       </BrowserRouter>
       <Footer />
---
+++ src/pages/Error404.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/components/error404.scss";
+
+const Erreur404 = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Oups! la page que vous demandez n'existe pas.</p>
+      <div className="lien-accueil">
+        <Link to="/" className="lien">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Erreur404;
