cc0636d4663a02511bb97e5d4c6bbdb024e388ec
zoulikha <zaz13@me.com>
2023-08-08 03:39:35 +0200

    add display menu responsive mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
 import "../styles/components/header.scss";
 import { Link } from "react-scroll";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
+  // Création d'une référence à l'élément "nav" du DOM pour accéder à ses propriétés
+  const navRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // false = menu fermé
+
+  // Fonction pour afficher ou masquer le menu (toggle)
+  const showNavbar = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header>
       <img className="logo" src="" alt="" />
-      <nav>
+      <nav ref={navRef} className={isMenuOpen ? "responsive_nav" : ""}>
         <div className="navigation">
           <ul>
             <li>
-              <Link to="accueil" smooth={true} duration={500}>
+              <Link
+                to="accueil"
+                smooth={true}
+                duration={500}
+                onClick={closeNavbar}
+              >
                 Accueil
               </Link>
             </li>
             <li>
-              <Link to="presentation" smooth={true} duration={500}>
+              <Link
+                to="presentation"
+                smooth={true}
+                duration={500}
+                onClick={closeNavbar}
+              >
                 Présentation
               </Link>
             </li>
             <li>
-              <Link to="competences" smooth={true} duration={500}>
+              <Link
+                to="competences"
+                smooth={true}
+                duration={500}
+                onClick={closeNavbar}
+              >
                 Compétences
               </Link>
             </li>
             <li>
-              <Link to="portfolio" smooth={true} duration={500}>
+              <Link
+                to="portfolio"
+                smooth={true}
+                duration={500}
+                onClick={closeNavbar}
+              >
                 Portfolio
               </Link>
             </li>
             <li>
-              <Link to="contact" smooth={true} duration={500}>
+              <Link
+                to="contact"
+                smooth={true}
+                duration={500}
+                onClick={closeNavbar}
+              >
                 Contact
               </Link>
             </li>
           </ul>
         </div>
       </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
     </header>
   );
 };
